9ba186d008f235cc9c3bf3701c91a11f127fa8e7
vivekrsingh10 <vksingh10may@gmail.com>
2025-11-01 11:10:53 +0530

    Frontend API call fix for Vercel

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: `${API_URL}/api`,
   withCredentials: true,
 });
