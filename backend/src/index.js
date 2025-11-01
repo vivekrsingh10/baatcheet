c73fe4359de2ec0710585f3540211fef6562d563
vivekrsingh10 <vksingh10may@gmail.com>
2025-11-01 10:29:20 +0530

    Backend Build Seperation from frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,11 +34,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  // app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  // });
 }
 
 server.listen(PORT, () => {
